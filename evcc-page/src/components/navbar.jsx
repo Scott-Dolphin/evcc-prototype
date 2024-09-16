b10815bd91702fb92b277a648800798397ac0091
Scott Dolphin <nathan-chen@csu.fullerton.edu>
2024-09-16 16:24:02 -0700

    updated theme, added assets

--- evcc-page/src/components/navbar.jsx
+++ evcc-page/src/components/navbar.jsx
@@ -1,11 +1,14 @@
-import logo from "./images/logo.jpg"
+// import logo from "./images/logo.jpg";
+import logo from "./images/logo.png";
+import banner_img from "./images/Website-parts/banner_img.jpg";
+
 
 export default function Navbar({ page_name, subtitle }) {
     return (
         <>
             <div class="Banner">
                 <div class="Navbar">
-                <a className="Logo" href="/"><img src={logo}/></a>
+                    <a className="logo" href="/"><img src={logo} /></a>
                     <li>
                         {/* todo: make these into buttons instead of links */}
                         <ul><a href="/mission">Mission</a></ul>
@@ -14,7 +17,9 @@ export default function Navbar({ page_name, subtitle }) {
                         <ul><a href="/connect">Connect</a></ul>
                     </li>
                 </div>
-                <div class="Spacer"></div>
+                <div class="Spacer">
+                    <img src={banner_img}/>
+                </div>
                 <div class="Title">
                     <h1>{page_name}</h1>
                     <h2>{subtitle}</h2>
